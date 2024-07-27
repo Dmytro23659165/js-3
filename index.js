42fe60f976ccc13d80971a4da45c28092b6bad52
Dmytro23659165 <130238497+Dmytro23659165@users.noreply.github.com>
2024-07-27 17:47:42 +0300

    js-3

---
+++ index.js
@@ -0,0 +1,23 @@
+let result;
+result = 5 + 5 + '5';
+console.log(result);
+
+
+let email = "dmytrobyrmehaandriy@gmail.com";
+let containsAt = email.includes("@");
+let emailcifry = email.length;
+console.log(containsAt);
+console.log(emailcifry);
+
+
+let slovo = "My ";
+let slowo = "name ";
+let slowa = "is ";
+let fullName = slovo + slowo + slowa + "victor";
+console.log(fullName);
+
+
+let userName = "Dima ";
+let payment = userName + "Дякуємо, До сплати 300 гривень";
+alert(payment);
+console.log(payment);
